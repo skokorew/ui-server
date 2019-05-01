bf6524f6a349e4586f10ec501ccf4b52d20c5227
Sergey Kokorev <skokorev@gmail.com>
2019-05-01 12:46:07 +0300

    added new button

--- src/main/java/ru/scrumtrek/uiserver/Server.java
+++ src/main/java/ru/scrumtrek/uiserver/Server.java
@@ -1,5 +1,6 @@
 package ru.scrumtrek.uiserver;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +22,13 @@ public class Server {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = {"/lines"})
-    public void setLines(@RequestBody String line) {
+    public ResponseEntity<Void> setLines(@RequestBody String line) {
+        if (lines.stream().anyMatch(l -> l.getLineChars().equals(line)))
+            return ResponseEntity.badRequest().build();
         lines.add(new Line(idInt.incrementAndGet(), line));
+        return ResponseEntity.ok().build();
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = {"/lines"})
+    public void removeAll() { lines.clear(); }
 }
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,15 +5,31 @@ var app = new Vue({
         newLineName: null
     },
     mounted() {
-        axios.get('/lines').then( value => {
-            this.lines = value.data;
-        })
+        this.refreshTable();
     },
     methods: {
+        refreshTable: function() {
+            axios.get('/lines').then( value => {
+                this.lines = value.data;
+            })
+        },
         addLine: function () {
             axios.post('/lines', this.newLineName, {
                 headers: { 'Content-Type': 'text/plain' }
-            });
+            }).
+            then(response => {
+                console.log(response)
+            }).
+            catch(error => {
+                if (error.response.status === 500) {
+                    console.log('Error!!!');
+                    alert('Error!!!')
+                }
+            }).finally(() => this.refreshTable());
+        },
+        removeAll: function() {
+            axios.delete('/lines');
+            this.refreshTable();
         }
     }
 });
